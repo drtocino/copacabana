de8e6349436469b6fec6e3fe8b9f068d935711e3
dilan504 <dilan_chuquimia@hotmail.com>
2021-05-10 23:39:35 -0400

    Cambio de metodo en envio de datos y validacion en al servidor, incializacion de sesiones (aun no se guardan)

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
-const app = express();
+const cors = require('cors');
 const mysql = require('mysql');
-//const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const app = express();
 
 const db = mysql.createPool({
     host: "127.0.0.1",
@@ -13,27 +15,69 @@ const db = mysql.createPool({
     database: "copacabana"
 });
 
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["POST","GET"],
+    credentials: true,
+}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/login/:usuario&:clave',(req,res) => {
-    const usuario = req.params.usuario;
-    const clave = req.params.clave;
-    const login = "SELECT * FROM usuario WHERE usuario = ? AND password = ?;";
+app.use(session({
+    key: 'userId',
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1800,
+    }
+}));
 
-    db.query(login, [usuario,clave], (error,result) => {
+app.get('/login',(req,res) => {
+    console.log(req.session)
+    if(req.session.user){
+        console.log("Logeado")
+        res.send({loggedIn: true,user: req.session.user});
+    }else{
+        res.send({loggedIn: false});
+    }
+});
+
+app.post('/login',(req,res) => {
+    const usuario = req.body.usuario;
+    const clave = req.body.clave;
+    const login = "SELECT * FROM usuario WHERE usuario = ?";
+    db.query(login,usuario,(error,result) => {
         if(error){
-            console.log("Ocurrio un ERROR"+error)
-        }else{
-            console.log("RESULTADOS:"+result)
-            if(result === ""){
-                console.log("no existe")
-                res.send("no existe");
-            }else{
-                res.send(result);
-            }
+            console.log("Ocurrio un error -> "+error)
         }
-    });
+        if (result.length > 0){
+            if(result[0].password.length < 16){
+                if (result[0].password === clave){
+                    req.session.user = result;
+                    res.send(result);
+                    console.log(req.session);
+                }else{
+                    res.send({message: "Combinacion incorrecta"})
+                    console.log("Combinacion incorrecta")
+                }
+            }else{
+                bcrypt.compare(clave, result[0].password,(er,re) => {
+                    if(re){
+                        res.send(result);
+                        console.log(result);
+                    }else{
+                        res.send({message: "Combinacion de contra incorrecta"});
+                        console.log('Combinacion de contra incorrecta'+re)
+                    }
+                })
+            }
+        }else{
+            console.log("No existe el usuario")
+            res.send({message:"No existe el usuario"})
+        }
+    })
 })
 
 app.listen(3001,() => {
