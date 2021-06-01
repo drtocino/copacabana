da2502440e04fc6742b6e0749e368d3da6245397
JosueQP <84089095+JosueQP@users.noreply.github.com>
2021-06-01 18:50:43 -0400

    agregando Update,delete

--- client/src/components/Bus.js
+++ client/src/components/Bus.js
@@ -110,46 +110,6 @@ const Buses = () => {
         });
       }
       
-      function App(){
-        var subtitle;
-        const [modalIsOpen,setIsOpen] = React.useState(false);
-        function openModal() {
-          setIsOpen(true);
-        }
-      
-        function afterOpenModal() {
-          // references are now sync'd and can be accessed.
-          subtitle.style.color = '#f00';
-        }
-      
-        function closeModal(){
-          setIsOpen(false);
-        }
-      
-          return (
-            <div>
-              <button onClick={openModal}>Open Modal</button>
-              <Modal
-                isOpen={modalIsOpen}
-                onAfterOpen={afterOpenModal}
-                onRequestClose={closeModal}
-                contentLabel="Example Modal"
-              >
-      
-                <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
-                <button onClick={closeModal}>close</button>
-                <div>I am a modal</div>
-                <form>
-                  <input />
-                  <button>tab navigation</button>
-                  <button>stays</button>
-                  <button>inside</button>
-                  <button>the modal</button>
-                </form>
-              </Modal>
-            </div>
-          );
-      }
       
       const sendUpdate = () =>{
 
